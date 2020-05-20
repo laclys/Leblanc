f02db9077385a73e9eb4b8956f28ca4d9b7b8cc4
laclys <laclys@126.com>
2020-05-20 23:32:22 +0800

    added

--- 1TwoSum.js
+++ 1TwoSum.js
@@ -1,3 +1,9 @@
+/* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+链接：https://leetcode-cn.com/problems/two-sum */
+
 const twoSum = function(nums, target) {
   let obj = {}
   let ret = []
