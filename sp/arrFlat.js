70d9237bf2b40b4b6f1ecac13b0af592197d183a
Lac <laclys@126.com>
2021-02-23 16:41:52 +0800

    added

---
+++ sp/arrFlat.js
@@ -0,0 +1,16 @@
+const flatArr = arr =>  arr.reduce((pre, cur) => pre.concat(Array.isArray(cur) ? flatArr(cur) : cur), [])
+
+
+const flatArr2 = (arr) => {
+  const ret = []
+  const stack = [].concat(arr)
+  while(stack.length !== 0) {
+    const tmp = stack.pop()
+    if (Array.isArray(temp)) {
+      stack.push(...temp)
+    } else {
+      ret.unshift(tmp)
+    }
+  }
+  return ret
+}
---
+++ sp/curry.js
@@ -0,0 +1,29 @@
+function curry (fn, currArgs) {
+
+  return function(...args) {
+      // let args = [].slice.call(arguments);
+      // 首次调用时，若未提供最后一个参数currArgs，则不用进行args的拼接
+      if (currArgs !== undefined) {
+          args = args.concat(currArgs);
+      }
+
+      // 递归调用
+      if (args.length < fn.length) {
+          return curry(fn, args);
+      }
+
+      // 递归出口
+      return fn(...args);
+  }
+}
+
+// function sum(a, b, c) {
+//   console.log('sum', a + b + c);
+// }
+
+// const fn = curry(sum);
+
+// fn(1, 2, 3); // 6
+// fn(1, 2)(3); // 6
+// fn(1)(2, 3); // 6
+// fn(1)(2)(3); 
